f800107317f595fd2321ee986e24a7f16ac002cd
Cyprien Autexier <cyprien.autexier@gmail.com>
2017-01-05 18:55:52 +0100

    drop text plain produces

---
+++ src/GeekLearning.SwashbuckleExtensions/DropTextPlainProduces.cs
@@ -0,0 +1,24 @@
+﻿using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace GeekLearning.SwashbuckleExtensions
+{
+    public class DropTextPlainProduces : IOperationFilter
+    {
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            if (operation.Produces.Count > 0)
+            {
+                var textPlain = operation.Produces.FirstOrDefault(produce => produce.Equals("text/plain", StringComparison.OrdinalIgnoreCase));
+                if (textPlain != null)
+                {
+                    operation.Produces.Remove(textPlain);
+                }
+            }
+        }
+    }
+}
