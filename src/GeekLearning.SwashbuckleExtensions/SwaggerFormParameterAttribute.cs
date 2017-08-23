a80b65b145675843dbf194382dc2b32145fea5ae
Cyprien Autexier <cyprien@autexier.me>
2017-08-23 14:06:45 +1000

    Fix attribute name

--- src/GeekLearning.SwashbuckleExtensions/SwaggerFormParameterAttribute.cs
+++ src/GeekLearning.SwashbuckleExtensions/SwaggerFormParameterAttribute.cs
@@ -6,14 +6,14 @@
     /// Thanks to jonnybi - https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/193
     /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
-    public sealed class SwaggerFormParameter : Attribute
+    public sealed class SwaggerFormParameterAttribute : Attribute
     {
         public string Name { get; private set; }
         public string Type { get; private set; }
         public string Description { get; set; }
         public bool IsRequired { get; set; }
 
-        public SwaggerFormParameter(string name, string type)
+        public SwaggerFormParameterAttribute(string name, string type)
         {
             Name = name;
             Type = type;
