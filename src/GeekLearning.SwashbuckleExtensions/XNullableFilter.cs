03e093990772740e06791a14c447c986ee65ca8e
Sandor <2660262+sandorfr@users.noreply.github.com>
2017-11-14 13:47:29 +1100

    add x-nullable extension (#32)

---
+++ src/GeekLearning.SwashbuckleExtensions/XNullableFilter.cs
@@ -0,0 +1,18 @@
+﻿using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace GeekLearning.SwashbuckleExtensions
+{
+    public class XNullableFilter : ISchemaFilter
+    {
+        public void Apply(Schema model, SchemaFilterContext context)
+        {
+            var nullableUnderlyingType = Nullable.GetUnderlyingType(context.SystemType);
+
+            model.Extensions.Add("x-nullable", nullableUnderlyingType != null || !context.SystemType.IsValueType);
+        }
+    }
+}
