4c08ad2c2828ef1b5bb27d3be38357f5f79fecd5
Cyprien Autexier <cyprien@autexier.me>
2017-11-15 11:01:59 +1100

    add a document filter to remove unwanted basepath which does not comply stricly with the spec

---
+++ src/GeekLearning.SwashbuckleExtensions/FixBasePathDocumentFilter.cs
@@ -0,0 +1,19 @@
+﻿using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace GeekLearning.SwashbuckleExtensions
+{
+    public class FixBasePathDocumentFilter : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, DocumentFilterContext context)
+        {
+            if (swaggerDoc.BasePath == "/")
+            {
+                swaggerDoc.BasePath = null;
+            }
+        }
+    }
+}
