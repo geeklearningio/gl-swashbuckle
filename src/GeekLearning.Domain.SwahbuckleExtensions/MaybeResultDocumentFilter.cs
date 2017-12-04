6c7fc577e1a822f3721d263387bdd229bd186f3b
Cyprien Autexier <cyprien@autexier.me>
2017-12-04 14:19:57 +1100

    add content type when mayberesult is detected

--- src/GeekLearning.Domain.SwahbuckleExtensions/MaybeResultDocumentFilter.cs
+++ src/GeekLearning.Domain.SwahbuckleExtensions/MaybeResultDocumentFilter.cs
@@ -42,6 +42,10 @@
                             okResponse.Schema = context.SchemaRegistry.GetOrRegister(responseType);
 
                             operation.Responses["200"] = okResponse;
+                            if (!operation.Produces.Contains("application/json"))
+                            {
+                                operation.Produces.Add("application/json");
+                            }
                         }
                     }
 
