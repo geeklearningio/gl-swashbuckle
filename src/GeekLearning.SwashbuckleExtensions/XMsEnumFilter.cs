d21e5da44cd9284166611ed036cca6745ee18a77
Cyprien Autexier <cyprien@autexier.me>
2018-03-20 12:17:11 +1100

    Add x-ms-enum filter

---
+++ src/GeekLearning.SwashbuckleExtensions/XMsEnumFilter.cs
@@ -0,0 +1,39 @@
+﻿using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace GeekLearning.SwashbuckleExtensions
+{
+    public class XMsEnumFilter : ISchemaFilter
+    {
+        public void Apply(Schema model, SchemaFilterContext context)
+        {
+            var typeInfo = context.SystemType.GetTypeInfo();
+
+            if (typeInfo.IsEnum)
+            {
+                var names = Enum.GetNames(context.SystemType);
+                var values = model.Enum;
+
+                model.Extensions.Add(
+                    "x-ms-enum",
+                    new
+                    {
+                        name = typeInfo.Name,
+                        modelAsString = values.First() is string,
+                        values = Enumerable
+                            .Range(0, names.Length)
+                            .Select(index => new
+                            {
+                                value = values[index],
+                                name = names[index]
+                            })
+                    });
+            }
+        }
+    }
+}
